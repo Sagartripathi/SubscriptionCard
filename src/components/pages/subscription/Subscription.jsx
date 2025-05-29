6dd804e29083a5e1751b2534321ebbd9e24d8ed9
Sagartripathi <tripathisagar1997@gmail.com>
2025-05-29 15:48:52 +0545

    fixed issue on card

--- src/components/pages/subscription/Subscription.jsx
+++ src/components/pages/subscription/Subscription.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Card from "./card";
+import Card from "./Card";
 export const Subscription = () => {
   return (
     <div className="flex flex-row items-center justify-center gap-5 h-full bg-slate-800 text-white">
